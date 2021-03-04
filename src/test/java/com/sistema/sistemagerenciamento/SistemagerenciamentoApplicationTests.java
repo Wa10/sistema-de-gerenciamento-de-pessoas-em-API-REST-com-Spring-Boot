434109e0ee0e1535cf7e28f31d3b16918646f2b1
backdeveloper <walace.alves10@gmail.com>
2021-03-03 23:52:53 -0300

    first commit

---
+++ src/test/java/com/sistema/sistemagerenciamento/SistemagerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.sistemagerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemagerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
